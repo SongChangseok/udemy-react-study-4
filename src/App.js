64e4080c85c5858bd5a112c2760905300e0c6a2e
cssong <a1373052011@gmail.com>
2022-07-15 09:29:11 +0900

    prerttier 적용 / useEffect를 이용한 login 플래그 생성

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedLoggedInInformation === "true") setIsLoggedIn(true);
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    console.log(email, password);
+    localStorage.setItem("isLoggedIn", true);
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
   return (
